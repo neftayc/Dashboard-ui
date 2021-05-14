ea22cea7e4038a955030bfd1a3c95eec680538d2
German Castro <germancvdev@gmail.com>
2021-05-14 00:20:42 -0500

    add notifications with

--- plugins/axios.js
+++ plugins/axios.js
@@ -4,25 +4,29 @@ export default function (ctx) {
     // const url = config.url
 
     const detail = data.detail || ''
-
+    const type = 'ERROR'
+    const title = 'Error!'
     switch (config.method) {
       case 'patch':
         ctx.store.commit('SHOW_SNACKBAR', {
-          color: 'error',
+          type,
+          title,
           message: detail ?? 'Error editing!',
         })
         break
 
       case 'post':
         ctx.store.commit('SHOW_SNACKBAR', {
-          color: 'error',
+          type,
+          title,
           message: detail ?? 'Error creating!',
         })
         break
 
       case 'delete':
         ctx.store.commit('SHOW_SNACKBAR', {
-          color: 'error',
+          type,
+          title,
           message: detail ?? 'Error deleting!',
         })
         break
@@ -38,7 +42,7 @@ export default function (ctx) {
     // const url = x.config.url
     // const query = ctx.route.name
     const type = 'SUCCESS'
-    const title = 'Success'
+    const title = 'Success!'
     switch (method) {
       case 'post':
         ctx.store.commit('SHOW_SNACKBAR', {
